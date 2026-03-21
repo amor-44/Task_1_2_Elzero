a5a2d671b7c52979044f136e20c9494ebebb1227
Amr Hesham <hamr6653@gmail.com>
2026-03-21 12:58:52 +0200

    Dom

--- week6/main.js
+++ week6/main.js
@@ -1,3 +1,5 @@
+const { use } = require("react");
+
 let member = {
   name: "Amr",
   age: 20,
@@ -108,3 +110,90 @@ for (let i = 0; i < objectLength; i++) {
 }
 
 //===================================================================================
+
+// dom
+
+let way1 = document.getElementById("elzero");
+let way2 = document.getElementsByClassName("element")[0];
+let way3 = document.getElementsByTagName("div")[0];
+let way4 = document.getElementsByName("js")[0];
+let way5 = document.querySelector("#elzero");
+let way6 = document.querySelector(".element");
+let way7 = document.querySelector("div");
+let way8 = document.querySelector("[name='js']");
+let way9 = document.querySelectorAll("#elzero")[0];
+let way10 = document.querySelectorAll(".element")[0];
+let way11 = document.querySelectorAll("div")[0];
+let way12 = document.querySelectorAll("[name='js']")[0];
+let way13 = document.body.firstElementChild;
+let way14 = document.body.children[0];
+let way15 = document.body.lastElementChild[0];
+
+let myImages = document.querySelectorAll("img");
+myImages.forEach((img) => {
+  img.src = "https://elzero.org/wp-content/themes/elzero/imgs/logo.png";
+  img.alt = "Elzero Logo";
+});
+
+let input = document.querySelector("[name='dollar']");
+let result = document.querySelector(".result");
+
+input.oninput = function () {
+  let usdValue = input.value;
+  let egpValue = (usdValue * 15.6).toFixed(2);
+  result.innerHTML = `{${usdValue}} USD Dollar = {${egpValue}} Egyptian Pound`;
+};
+
+let one = document.querySelector(".one");
+let two = document.querySelector(".two");
+let tempText = one.textContent;
+let tempTitle = one.title;
+
+one.textContent = two.textContent;
+one.title = two.title;
+
+two.textContent = `${tempText} ${two.attributes.length}`;
+two.title = tempTitle;
+
+let allImgs = document.querySelectorAll("img");
+
+allImgs.forEach((img) => {
+  if (img.hasAttribute("alt")) {
+    img.alt = "Old";
+  } else {
+    img.setAttribute("alt", "Elzero New");
+  }
+});
+
+let myForm = document.forms[0];
+let inputNum = document.querySelector("[name='elements']");
+let inputText = document.querySelector("[name='texts']");
+let selectType = document.querySelector("[name='type']");
+let results = document.querySelector(".results");
+
+myForm.onsubmit = function (e) {
+  e.preventDefault();
+
+  results.innerHTML = "";
+
+  for (let i = 1; i <= inputNum.value; i++) {
+    let myElement = document.createElement(selectType.value);
+
+    myElement.className = "box";
+    myElement.title = "Element";
+    myElement.id = `id-${i}`;
+
+    myElement.textContent = inputText.value;
+
+    myElement.style.display = "inline-block";
+    myElement.style.width = "150px";
+    myElement.style.padding = "10px";
+    myElement.style.margin = "10px";
+    myElement.style.backgroundColor = "#ff5722";
+    myElement.style.color = "white";
+    myElement.style.textAlign = "center";
+    myElement.style.borderRadius = "5px";
+
+    results.appendChild(myElement);
+  }
+};
